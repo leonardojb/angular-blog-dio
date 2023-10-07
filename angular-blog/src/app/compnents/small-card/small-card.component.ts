53a384cdc5a90e0c7cc2fa18841b424361c04ed1
leonardojb <leonardo.bruno@alumni.usp.br>
2023-10-07 01:39:03 -0300

    Dinamização dos componentes e correções de responsividade

--- angular-blog/src/app/compnents/small-card/small-card.component.ts
+++ angular-blog/src/app/compnents/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css', './small-card.responsive.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  smallCardPhotoCover:string =''
+  @Input()
+  smallCardTitle:string =''
+  @Input()
+  smallCardDescription:string =''
 
   constructor() { }
 
